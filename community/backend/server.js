17eb3c28cecddf9f7c76a9f56a7333718c519b23
Aman <amanchopra.atg@gmail.com>
2022-03-21 19:13:22 +0530

    started backend

---
+++ community/backend/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const PORT = 80
+
+// Middleware
+app.use(bodyParser.json({limit: "50mb"}))
+app.use(bodyParser.urlencoded({extended: "true", limit: "50mb"}))
+
+// resolving CORS Issue
+app.use((req, res, next) => {
+    req.headers("Access-Control-Allow-Origin", "*")
+    req.headers("Access-Control-Allow-Headers", "*")
+    next()
+})
+
+// routes
+
+
+app.use('/uploads', express.static(path.join(__dirname, "/../uploads")))
+app.use('/uploads', express.static(path.join(__dirname, "/../frontend/build")))
+
+app.get("*", (req, res) => {
+    try {
+        res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`))
+    } catch (e) {
+        res.send("Oops!! Unexpected Error :( ")
+    }
+})
+
+app.use(cors())
+
+// server listening
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Listening on Port Number ${PORT}`);
+})
